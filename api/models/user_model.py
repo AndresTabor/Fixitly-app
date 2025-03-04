88429a91fd6d60ef4bfac9e9767d5f31f2c5cd8a
Andres Taborda <andrestaborda43@gmail.com>
2025-03-04 19:22:35 +0100

    agregando roles en user_model

--- api/models/user_model.py
+++ api/models/user_model.py
@@ -1,5 +1,11 @@
 from datetime import datetime, timezone
 from config.bd_config import db
+from enum import Enum as PyEnum 
+
+class User_Role(str, PyEnum):
+    ADMIN = "admin"
+    CLIENT = "client"
+    PROFESSIONAL = "professional"
 
 class User(db.Model):
     __tablename__ = "users"
@@ -11,6 +17,7 @@ class User(db.Model):
     image = db.Column(db.String(200), unique = False, nullable = False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
     date_at = db.Column(db.DateTime, unique = False, nullable = False, default=datetime.now(timezone.utc))
+    role = db.Column(db.Enum(User_Role, name="user_role_enum"), nullable=False, default=User_Role.CLIENT)
 
     def __repr__(self):
         return f'<User {self.fullname}>'
